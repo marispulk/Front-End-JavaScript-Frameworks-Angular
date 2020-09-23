6de83b3ef7a09e1608d4cebfe33516d05b075da1
Maris Pulk <marispulk@gmail.com>
2020-09-23 13:56:58 +0300

    Directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -37,6 +37,7 @@ import { LeaderService } from "./services/leader.service";
 import { ProcessHTTPMsgService } from "./services/process-httpmsg.service";
 
 import { baseURL } from './shared/baseurl';
+import { HighlightDirective } from './directives/highlight.directive';
 
 @NgModule({
   declarations: [
@@ -48,7 +49,8 @@ import { baseURL } from './shared/baseurl';
     HomeComponent,
     AboutComponent,
     ContactComponent,
-    LoginComponent
+    LoginComponent,
+    HighlightDirective
 
 
   ],
