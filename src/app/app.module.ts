d9bda13d518dc925cdf77346f118c9a083321192
Maris Pulk <marispulk@gmail.com>
2020-09-23 12:34:32 +0300

    HTTP part 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -34,6 +34,7 @@ import { LoginComponent } from './login/login.component';
 import { DishService } from "./services/dish.service";
 import { PromotionService } from "./services/promotion.service";
 import { LeaderService } from "./services/leader.service";
+import { ProcessHTTPMsgService } from "./services/process-httpmsg.service";
 
 import { baseURL } from './shared/baseurl';
 
@@ -77,6 +78,7 @@ import { baseURL } from './shared/baseurl';
     DishService,
     PromotionService,
     LeaderService,
+    ProcessHTTPMsgService,
     {provide: 'BaseURL', useValue: baseURL}
 
   ],
